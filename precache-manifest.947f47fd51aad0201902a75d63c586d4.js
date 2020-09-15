42ca97b1c28daea3a44de5f3d3f4e639a9f78084
Raj Sharma <xrehpicx@gmail.com>
2020-09-15 20:16:42 +0530

    Updates

---
+++ precache-manifest.947f47fd51aad0201902a75d63c586d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a023afebb9905f9c2843b1ccab5ba1d",
+    "url": "/aniruddh/index.html"
+  },
+  {
+    "revision": "3ed62d213087c441b601",
+    "url": "/aniruddh/static/css/main.2e28c845.chunk.css"
+  },
+  {
+    "revision": "1e9fd88b40c1e3bf2e57",
+    "url": "/aniruddh/static/js/2.c6562a53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aniruddh/static/js/2.c6562a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed62d213087c441b601",
+    "url": "/aniruddh/static/js/main.10e9164b.chunk.js"
+  },
+  {
+    "revision": "31ba987b6898c31fba91",
+    "url": "/aniruddh/static/js/runtime-main.c9cc6c52.js"
+  }
+]);
